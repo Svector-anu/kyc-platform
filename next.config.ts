4336db286daba62bf9d32d76d0997b26b573d75e
svector-anu <anuoluwakolapo94@gmail.com>
2025-11-06 16:45:53 +0100

    Update next.config.ts to expand connect-src directives, adding support for WalletConnect and Coinbase domains, enhancing connectivity options for the application.

--- next.config.ts
+++ next.config.ts
@@ -30,7 +30,7 @@ const nextConfig = {
                 "img-src 'self' data: https: blob:",
                 "font-src 'self' data:",
                 // ADD YOUR IP HERE ↓
-                "connect-src 'self' http://localhost:* ws://localhost:* http://172.20.10.14:* ws://172.20.10.14:* https://*.alchemy.com https://*.sepolia.io https://*.infura.io wss://*.infura.io https://*.polygonid.me",
+                "connect-src 'self' http://localhost:* ws://localhost:* http://172.20.10.14:* ws://172.20.10.14:* https://*.alchemy.com https://*.sepolia.io https://*.infura.io wss://*.infura.io https://*.polygonid.me https://*.web3modal.org https://*.walletconnect.com https://*.walletconnect.org wss://*.walletconnect.com wss://*.walletconnect.org https://rpc.walletconnect.com https://*.coinbase.com https://cca-lite.coinbase.com",
                 "frame-src 'self'",
                 "worker-src 'self' blob:",
               ].join('; '),
